72c4230fce6ea94d87ade5fb00cff9e8f9eef78e
Matthew Goslett <matthew@goslett.co.za>
2016-04-04 11:17:16 +0200

    initial release

---
+++ README.md
@@ -0,0 +1,42 @@
+# A Monolog extension for formatting log entries for Google Cloud Logging
+
+[![Author](http://img.shields.io/badge/author-@superbalist-blue.svg?style=flat-square)](https://twitter.com/superbalist)
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](LICENSE)
+[![Packagist Version](https://img.shields.io/packagist/v/superbalist/monolog-google-cloud-json-formatter.svg?style=flat-square)](https://packagist.org/packages/superbalist/monolog-google-cloud-json-formatter)
+[![Total Downloads](https://img.shields.io/packagist/dt/superbalist/monolog-google-cloud-json-formatter.svg?style=flat-square)](https://packagist.org/packages/superbalist/monolog-google-cloud-json-formatter)
+
+
+This library works in conjunction with https://github.com/GoogleCloudPlatform/fluent-plugin-google-cloud
+
+fluent-plugin-google-cloud is an output plugin for fluentd which sends logs to the Google Cloud Logging API.
+
+
+This formatter outputs logs entries in json which the fluent-plugin-google-cloud application understands.
+
+## Installation
+
+```bash
+composer require superbalist/monolog-google-cloud-json-formatter
+```
+
+## Usage
+
+```php
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Superbalist\Monolog\Formatter;
+
+// create a handler
+$handler = new StreamHandler('path/to/your.log', Logger::WARNING);
+
+// use custom formatter in handler
+$handler->setFormatter(new GoogleCloudJsonFormatter());
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler($handler);
+
+// add records to the log
+$log->addWarning('Foo');
+$log->addError('Bar');
+```
---
+++ src/GoogleCloudJsonFormatter.php
@@ -0,0 +1,62 @@
+<?php namespace Superbalist\Monolog\Formatter;
+
+use Monolog\Formatter\JsonFormatter;
+
+class GoogleCloudJsonFormatter extends JsonFormatter
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function format(array $record)
+    {
+        return json_encode(
+            $this->translateRecordForGoogleCloudLoggingFormat($record)
+        ) . ($this->appendNewline ? "\n" : '');
+    }
+
+    /**
+     * Return a JSON-encoded array of records.
+     *
+     * @param array $records
+     * @return string
+     */
+    protected function formatBatchJson(array $records)
+    {
+        $records = array_map(
+            function ($record) {
+                return $this->translateRecordForGoogleCloudLoggingFormat($record);
+            },
+            $records
+        );
+        return json_encode($records);
+    }
+
+    /**
+     * @param array $record
+     * @return array
+     */
+    protected function translateRecordForGoogleCloudLoggingFormat(array $record)
+    {
+        // translate the data into a format which google's out_google_cloud.rb plugin can understand
+        // see https://github.com/GoogleCloudPlatform/fluent-plugin-google-cloud/blob/master/lib/fluent/plugin/out_google_cloud.rb
+
+        $dt = $record['datetime'];
+        /** @var \DateTime $dt */
+
+        $formatted = [
+            'message' => $record['message'],
+            'severity' => $record['level_name'],
+            'timestamp' => [
+                'seconds' => $dt->getTimestamp(),
+                'nanos' => 0,
+            ],
+            'channel' => $record['channel']
+        ];
+
+        // merge in anything else from context and extra as structured metadata
+        $formatted = array_merge($record['context'], $record['extra'], $formatted);
+
+        return $formatted;
+    }
+}
